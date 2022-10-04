0261f45b7e9819a24620b25ef7f2182ea5f354d0
Volodymyr Bodnar <volodymyr.bodnar@collibra.com>
2022-10-04 23:01:38 +0200

    example

--- src/main/java/com/lj/LearningJavaApp.java
+++ src/main/java/com/lj/LearningJavaApp.java
@@ -4,5 +4,16 @@ public class LearningJavaApp {
     public static void main(String[] args){
         System.out.println("Hello World");
 //        asdfasfasf
+
+
+        String[] intArray = new String[5];
+        intArray[0] = "hgjhgf";
+        intArray[1] = "12";
+        intArray[2] = "3";
+        intArray[4] = "6";
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
     }
 }
